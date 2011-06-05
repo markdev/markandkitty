70cf66a39a6280ec421ee261b32db21c9fc6e40d
Mark Karavan <mark.karavan@gmail.com>
2011-06-05 15:36:07 -0400

    added CMS info

---
+++ config/site.php
@@ -0,0 +1,8 @@
+<?php 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'firecircus');
+define('BASE_URL', 'http://localhost:85');
+define('DIR_REL', '/markandkitty');
+define('PASSWORD_SALT', 'pBMzcUxoUeRAZfQaQeIhVwtQFuk7S4uLyrkenGYNcRzxWBD2bU1BWaXe1rYgiBNo');
